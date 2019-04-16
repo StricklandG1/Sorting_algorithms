872889f2fbf1f9bfae67b46f37a896d04569a84a
StricklandG1 <46575820+StricklandG1@users.noreply.github.com>
2019-04-16 13:52:45 -0700

    added rand number generator to main

--- CS_165/main.cpp
+++ CS_165/main.cpp
@@ -1,7 +1,51 @@
+#include "project1.h"
+
 #include <iostream>
+#include <ctime>
+#include <random>
+
+void swap(int& a, int& b);
+void print(int* a, int size);
 
 int main()
 {
+	int size = 10;
+	int* a = new int[size];
+	for (int i = 0; i < size; ++i)
+	{
+		a[i] = i + 1;
+	}
+
+	print(a, size);
+
+	std::random_device rd;
+	std::mt19937 eng(rd());
+	std::uniform_int_distribution<> distr(0, size - 1);
+	for (int i = 0; i < size; ++i)
+	{
+		swap(a[i], a[distr(eng)]);
+	}
+
+	print(a, size);
+
+	delete[] a;
+
 	system("pause");
 	return 0;
+}
+
+void swap(int& a, int& b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void print(int* a, int size)
+{
+	for (int i = 0; i < size; ++i)
+	{
+		std::cout << a[i] << " ";
+	}
+	std::cout << std::endl;
 }
--- CS_165/project1.h
+++ CS_165/project1.h
@@ -1,6 +1,10 @@
+#ifndef PROJECT1_H
+#define PROJECT1_H
 
 void bubble_sort(int* a, int size);
 void insertion_sort(int* a, int size);
 void spin_the_bottle_sort(int* a, int size);
 void shell_sort(int* a, int size, const int* gaps, int gap_size);
 void annealing_sort(int* a, int size, const int* temps, int temp_size, const int* reps, int rep_size);
+
+#endif
