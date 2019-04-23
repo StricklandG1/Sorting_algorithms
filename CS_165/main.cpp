d4dba60665f84046d525b0abad65f7bfe7b763dd
StricklandG1 <46575820+StricklandG1@users.noreply.github.com>
2019-04-23 12:55:32 -0700

    added almost sorted permutations and timings

--- CS_165/main.cpp
+++ CS_165/main.cpp
@@ -1,12 +1,16 @@
 #include "project1.h"
 
 #include <iostream>
+#include <fstream>
 #include <random>
+#include <ctime>
+#include <string>
 
 void swap(int& a, int& b);
-void test_set(int test_size);
+void test_set(int test_size, bool almost_sorted);
 void shell_sequence(std::vector<int>& a, int size);
 std::ostream& operator<<(std::ostream& out, const std::vector<int>& a);
+void write_to_file(std::string fName, int n, double t);
 
 const int SMALL_SET = 100;
 const int MEDIUM_SET = 1000;
@@ -16,7 +20,18 @@ const int PHAT_ASS_SET = 1000000;
 
 int main()
 {
-	//test_set(SMALL_SET);
+	/*test_set(SMALL_SET, false);
+	test_set(SMALL_SET, false);
+	test_set(SMALL_SET, false);
+	test_set(SMALL_SET, true);
+	test_set(SMALL_SET, true);
+	test_set(SMALL_SET, true);*/
+	test_set(MEDIUM_SET, false);
+	test_set(MEDIUM_SET, false);
+	test_set(MEDIUM_SET, false);
+	test_set(MEDIUM_SET, true);
+	test_set(MEDIUM_SET, true);
+	test_set(MEDIUM_SET, true);
 	//test_set(MEDIUM_SET);
 	//test_set(LARGE_SET);
 	//test_set(EVEN_LARGER_SET);
@@ -33,7 +48,7 @@ void swap(int& a, int& b)
 	b = temp;
 }
 
-void test_set(int set_size)
+void test_set(int set_size, bool almost_sorted)
 {
 	std::vector<int> a = std::vector<int>(set_size);
 	std::vector<int> b = std::vector<int>(set_size);
@@ -46,53 +61,68 @@ void test_set(int set_size)
 	std::random_device rd;
 	std::mt19937 eng(rd());
 	std::uniform_int_distribution<> distr(0, set_size - 1);
-	for (int i = 0; i < set_size; ++i)
+	if (!almost_sorted)
 	{
-		swap(a[i], a[distr(eng)]);
+		for (int i = 0; i < set_size; ++i)
+		{
+			swap(a[i], a[distr(eng)]);
+		}
+	}
+	else
+	{
+		int x = 0;
+		for (int i = set_size; i != 0; i /= 2)
+		{
+			++x;
+		}
+		x *= 2;
+
+		for (int i = 0; i < x; ++i)
+		{
+			swap(a[distr(eng)], a[distr(eng)]);
+		}
 	}
 
 	b = a;
 
 	std::cout << "-------------------- BUBBLE SORT --------------------\n\n";
-	std::cout << "BEFORE:\n";
-	std::cout << b;
+	double t = 0.0;
+	clock_t start = clock();
 	bubble_sort(b);
-
-	std::cout << "AFTER:\n";
-	std::cout << b;
+	clock_t end = clock();
+	t = static_cast<float>(end - start) / CLOCKS_PER_SEC;
+	std::string fName = (almost_sorted) ? "bubble_almost" : "bubble_random";
+	write_to_file(fName, set_size, t);
 
 	b = a;
 	std::cout << "-------------------- INSERTION SORT --------------------\n\n";
-	std::cout << "BEFORE:\n";
-	std::cout << b;
+	start = clock();
 	insertion_sort(b);
-
-	std::cout << "AFTER:\n";
-	std::cout << b;
+	end = clock();
+	t = static_cast<float>(end - start) / CLOCKS_PER_SEC;
+	fName = (almost_sorted) ? "insertion_almost" : "insertion_random";
+	write_to_file(fName, set_size, t);
 
 	b = a;
 	std::cout << "------------------- SPIN THE BOTTLE -------------------\n\n";
-	std::cout << "BEFORE:\n";
-	std::cout << b;
+	start = clock();
 	spin_the_bottle_sort(b);
-
-	std::cout << "AFTER:\n";
-	std::cout << b;
+	end = clock();
+	t = static_cast<float>(end - start) / CLOCKS_PER_SEC;
+	fName = (almost_sorted) ? "spin_almost" : "spin_random";
+	write_to_file(fName, set_size, t);
 
 	b = a;
 	std::cout << "------------------- SHELL SORT -------------------\n\n";
-	std::cout << "BEFORE:\n";
-	std::cout << b;
 	
 	std::vector<int> gaps;
 	shell_sequence(gaps, set_size);
-	std::cout << gaps;
+	start = clock();
 	shell_sort(b, gaps);
-	
-	std::cout << "AFTER:\n";
-	std::cout << b;
-
-	
+	end = clock();
+	t = static_cast<float>(end - start) / CLOCKS_PER_SEC;
+	fName = (almost_sorted) ? "shell_almost" : "shell_random";
+	write_to_file(fName, set_size, t);
 }
 
 void shell_sequence(std::vector<int>& a, int size)
@@ -112,4 +142,11 @@ std::ostream& operator<<(std::ostream& out, const std::vector<int>& a)
 	}
 	out << "\n\n";
 	return out;
+}
+
+void write_to_file(std::string fName, int n, double t)
+{
+	std::ofstream outFile;
+	outFile.open(fName + ".txt", std::ofstream::app);
+	outFile << fName << "," << n << "," << t << "\n";
 }
